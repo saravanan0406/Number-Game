fa7c004b01e737cf0626d0c5a00e947630bab646
saravanan0406 <sarovijayms@gmail.com>
2024-06-28 14:55:04 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+const random = Math.floor(Math.random()*10 + 1)
 
 function App() {
+
+  const [input,setinput] = useState()
+  let res;
+  let rres;
+
+  if(input){
+  if(input > random){
+    res='Heigher'
+  }
+  else if(input < random){
+    res='Lower'
+  }
+  else if(input==random){
+    res='Correct !!!'
+  
+  }
+  else{
+     rres='Enter a valid Field'
+  }
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Number Game</h2>
+      <div className="container">
+        <h4>Try to guess the number between 1 to 10</h4>
+        <input type='text' onChange={(e)=>setinput(e.target.value)}/>
+        {res?<h4>You Guesses is : {res}</h4>:<h4>{rres}</h4>}
+      </div>
     </div>
   );
 }
